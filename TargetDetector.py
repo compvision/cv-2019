135398cc6339c35aeb50c8d236a127fee1225e55
austin-li <austinmingli@gmail.com>
2019-01-18 17:10:09 -0800

    fixed formatting and removed useless stuff

--- TargetDetector.py
+++ TargetDetector.py
@@ -2,8 +2,6 @@ import numpy as np
 import cv2
 class TargetDetector:
     lightblue = (255, 221, 0)                                   # variable that stands for the lightblue color
-    contours = None                                             # variable that will hold the array of contours
-    index = None                                                # index variable that will hold the index of the proper contour
     corners = None                                              # variable that holds the array of corner points
     leftRect = None
     rightRect = None
@@ -14,24 +12,21 @@ class TargetDetector:
         pass
 
 # method that converts frame to HSV and then returns new frame of thresholded values
-    def threshold(self,min,max,frame):
+    def threshold(self, min, max, frame):
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         thresh = cv2.inRange(hsv, min, max)
         return thresh
 
 # method that finds and sets the contours to the variable contours for later usage
-    def Contours(self,threshold):
+    def contours(self, threshold):
         self.contours = cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
-
-    
 
 # method that iterates through each contour in the contours array and filters them by number and size
     def filterContours(self):
-        for c in range(len(self.contours)):
-            epsilon = 0.01 * cv2.arcLength(self.contours[c],True)
-            approx = cv2.approxPolyDP(self.contours[c],epsilon, True)
-            if(len(approx)==4 and cv2.contourArea(approx)>1000):
-                self.index = c
+        for c in self.contours:
+            epsilon = 0.01 * cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c ,epsilon, True)
+            if(len(approx) == 4 and cv2.contourArea(approx) > 1000):
                 self.corners = approx
                 if self.isLeftRect(approx):
                     self.leftRect = approx
@@ -60,5 +55,5 @@ class TargetDetector:
 
 
 # getter method that returns the contours array, index of significant contour, and the corners array
-    def getContours(self):
-        return self.contours,self.index,self.corners, self.isCorrectOrientation
+    def getCorners(self):
+        return self.corners
