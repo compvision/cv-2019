93f7e87ee9cac94f289047075c69bdab6a3e1975
austin-li <austinmingli@gmail.com>
2019-01-21 16:12:29 -0800

    changed variable names

--- Target.py
+++ Target.py
@@ -1,45 +1,45 @@
 import math
 
 class Target:
-    Xmax = 0                                                # will hold the value of the maximum X value
-    Ymax = 0                                                # will hold the value of the maximum Y value
-    Xmin = 1000                                             # will hold the value of the minumum X value
-    Ymin = 1000                                             # will hold the value of the minimum Y value
-    Xmid = 0                                                # will hold the X value of the center point
-    Ymid = 0                                                # will hold the Y value of the center point
+    xMax = 0                                                # will hold the value of the maximum X value
+    yMax = 0                                                # will hold the value of the maximum Y value
+    xMin = 1000                                             # will hold the value of the minumum X value
+    yMin = 1000                                             # will hold the value of the minimum Y value
+    xMid = 0                                                # will hold the X value of the center point
+    yMid = 0                                                # will hold the Y value of the center point
 
 # default initial method that initializes variables stated above
     def __init__(self, corners, isLeft):
         for corner in corners:                                # for loop that iterates through each corner array in the given array
             x = corner[0][0]
             y = corner[0][1]
-            if self.Xmax < x:                     # series of if statements that get the minimum and maximum x and y values of the corners
-                self.Xmax = x
-            if self.Xmin > x:
-                self.Xmin = x
-            if self.Ymax < y:
-                self.Ymax = y
-            if self.Ymin > y:
-                self.Ymin = y
+            if self.xMax < x:                     # series of if statements that get the minimum and maximum x and y values of the corners
+                self.xMax = x
+            if self.xMin > x:
+                self.xMin = x
+            if self.yMax < y:
+                self.yMax = y
+            if self.yMin > y:
+                self.yMin = y
         self.hypotenuse = math.hypot(corners[0][0][0] - corners[2][0][0], corners[0][0][1] - corners[2][0][1])
-        self.Xmid = int((self.Xmax + self.Xmin)/2)
-        self.Ymid = int((self.Ymax + self.Ymin)/2)
+        self.xMid = int((self.xMax + self.xMin)/2)
+        self.yMid = int((self.yMax + self.yMin)/2)
         self.isLeft = isLeft
 
  # getter method for the center x and y values
     def getCenter(self):
-        return (self.Xmid, self.Ymid)
+        return (self.xMid, self.yMid)
 
  # getter method for the width of the target
     def getWidth(self):
-        return self.Xmax - self.Xmin
+        return self.xMax - self.xMin
 
  # getter method for the height of the target
     def getHeight(self):
-        return self.Ymax - self.Ymin
+        return self.yMax - self.yMin
 
     def calculateTargetCenter(self):
         delta = self.hypotenuse/math.hypot(2, 5.5) * 4
         if self.isLeft:
-            return self.Xmax + delta
-        return self.Xmin - delta
+            return self.xMax + delta
+        return self.xMin - delta
